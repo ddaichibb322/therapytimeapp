01df046ef08e6df37e4aa89b9e6c62f4be6ad9d2
D <ddaichibb322@gmail.com>
2020-02-02 22:07:11 +0900

    aaaaaaa

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.template_top')
 
 @section('title', 'トップページ')
-@section('description', 'TherapyTime(セラピータイム)は、あなたの心に「ゆとり」をもたらすオンライン瞑想サービスです。専門家監修の動画コンテンツを用いて、いつでも何度でも手軽に本格瞑想に取り組むことができる、新しい瞑想サービスです。')
+@section('description', 'TherapyTime(セラピータイム)は、あなたの心に「ゆとり」をもたらすオンライン瞑想アプリです。専門家監修の動画コンテンツを用いて、いつでも何度でも手軽に本格瞑想に取り組むことができる、新しい瞑想サービスです。')
 
 @include('layouts.head')
 
@@ -36,7 +36,7 @@
 </section> -->
 <section id="scroll-target" class="top-section">
     <h2 class="top-section-title">瞑想に、効果はない？</h2>
-    <p class="top-section-text"> 「瞑想に効果はあるのか？」と思っていませんか？<br>　日本でまだ瞑想は発展途上ですが、海外では世界的企業のトップやセレブ、プロスポーツ選手などの間で瞑想が大変な人気となっています。<br> そのような成功者たちがこぞって瞑想に取り組むのには、「精神力を向上させて、気持ちに余裕を持たせ、パフォーマンスを高める」という理由が多いです。<br>　世界的に見れば、情報やモノが溢れかえるこの現代に、今の自分自身を打ち破り、理想の人生を手に入れるようとして瞑想に取り組む人は非常に多くいます。<br> 瞑想の効果は、科学的にも証明されていますので、あなたもぜひ一度取り組んでみてはいかがでしょうか。
+    <p class="top-section-text"> &nbsp;&nbsp;あなたは、<b>「瞑想ってほんとに効果はあるの？」と思っていませんか？</b><br>&nbsp;&nbsp;確かに、日本ではまだ瞑想はマイナーで、発展途上です。また目に見えない精神的な効果を実感するのは、実際にやってみないと難しいところもあります。<br>&nbsp;&nbsp;しかし、海外では<b>世界的企業のトップやセレブ、プロスポーツ選手などの間で瞑想が大変な人気</b>となっています。<br> そのような成功者たちがこぞって瞑想に取り組むのには、「気持ちに余裕を持たせ、パフォーマンスを高める」という理由が多いです。<br>&nbsp;&nbsp;世界的に見れば、情報やモノが溢れかえるこの現代に、<b>今の自分自身を打ち破り、理想の人生を手に入れるよう</b>として瞑想に取り組む人は非常に多くいます。<br>&nbsp;&nbsp;<b>瞑想の効果は、科学的にも証明されています</b>ので、あなたもぜひ一度取り組んでみてはいかがでしょうか。
     </p>
 </section>
 <section class="top-section--bg-gray">
@@ -47,24 +47,21 @@
                 <h3 class="top-section__reason-title">負の感情を抑える</h3>
                 <img class="top-section__reason-img" src="/img/relax-2.jpg" alt="reason1">
             </div>
-            <p class="top-section__reason-desc">ストレスや不安といった、心を悩ませる負の感情を沈めます。瞑想を通して、あなたを悩ますネガティブな感情や雑念を取り除き、クリアな気持ちを保つことで、より豊かな人生を歩んでいくことができます。</p>
+            <p class="top-section__reason-desc">&nbsp;&nbsp;ストレスや不安といった、<b>心の重荷となる負の感情を沈めます。</b><br>&nbsp;&nbsp;瞑想を通して、あなたを悩ますネガティブな感情や雑念を取り除き、クリアな気持ちを保つことで、<b>仕事や生活において、最大限パフォーマンスを発揮し、より高い成果を得ることにつながります。</b></p>
         </div>
         <div class="top-section__reason">
             <div class="top-section__reason-img-container">
                 <h3 class="top-section__reason-title">集中力を高める</h3>
                 <img class="top-section__reason-img" src="/img/concentration.jpg" alt="reason1">
             </div>
-
-            <p class="top-section__reason-desc">科学的に、瞑想によって集中力が高まることがわかっています。仕事や趣味、人間関係において理想とする成果を得るために、瞑想が活用されています。</p>
+            <p class="top-section__reason-desc">&nbsp;&nbsp;科学的に、<b>瞑想によって集中力が高まることがわかっています。</b><br>&nbsp;&nbsp;海外をはじめ、日本国内でも福利厚生として瞑想サービスを取り入れる企業が目立ち始めています。<br>&nbsp;&nbsp;<b>仕事や趣味、人間関係において理想とする成果を得るために、瞑想が活用されています。</b></p>
         </div>
         <div class="top-section__reason">
-
             <div class="top-section__reason-img-container">
                 <h3 class="top-section__reason-title">自己肯定感を高める</h3>
                 <img class="top-section__reason-img" src="/img/self-esteem-2.jpg" alt="reason1">
             </div>
-
-            <p class="top-section__reason-desc">瞑想を通して、過去のトラウマや失敗体験、恥ずかしい記憶といったものから逃げずに受け入れ、自身を肯定する。それによって、確固たる自信を築いていくことができます。</p>
+            <p class="top-section__reason-desc">&nbsp;&nbsp;瞑想を通して、過去のトラウマや失敗体験、恥ずかしい記憶といったものから逃げずに受け入れ、自身を肯定することができます。<br>&nbsp;&nbsp;自分の可能性を狭める「嫌な体験/記憶」も、受け入れることで、自分自身を肯定することができ、<b>確固たる自信</b>を築いていくことができます。</p>
         </div>
         <div class="top-section__reason">
 
@@ -72,7 +69,7 @@
                 <h3 class="top-section__reason-title">潜在能力を引き出す</h3>
                 <img class="top-section__reason-img" src="/img/potential-2.jpg" alt="reason1">
             </div>
-            <p class="top-section__reason-desc">人は得てして自分の可能性を自分で過少に規定しています。瞑想を通して、自分の発揮できる潜在能力に対する心理的な障壁を取り除き、自身の持てる才能を開花させることができます。</p>
+            <p class="top-section__reason-desc">&nbsp;&nbsp;人は得てして自分の可能性を自分で過少に規定しています。<br>&nbsp;&nbsp;その思い込みによって、本来発揮できる能力の一部しか顕在化していない可能性が高いのです。<br>&nbsp;&nbsp;瞑想を通して、自分の潜在能力に対する心理的な障壁を取り除き、<b>自身の持てる才能を開花させることができます。</b></p>
         </div>
     </div>
 </section>
@@ -81,15 +78,15 @@
     <h2 class="top-section-title">セラピータイムってどんなアプリ？</h2>
     <div class="top-section__merits">
         <div class="top-section__merit-container">
-            <h3 class="top-section__merit-title">動画教材で、だれでもすぐに本格瞑想！</h3>
+            <h3 class="top-section__merit-title">動画コンテンツで、費用を抑えて本格瞑想！</h3>
             <div class="top-section__merit-img-container"><img class="top-section__merit-img--1" src="/img/top-merit1.svg" alt=""></div>
 
-            <p class="top-section__merit-desc">セラピータイムは瞑想を誰でも簡単に行えるような動画コンテンツをご提供しています。高額なお金を払う必要はありません。店舗で行う瞑想は賃料や人件費で必要以上の価格を払う必要がありますし、独学では正しい方法にたどり着くのに時間がかかりすぎます。これから瞑想を始める方も、すでに取り組んでいる方も、セラピータイムなら専門家の指導下で、お手頃な値段で本格的な瞑想に取り組めます。</p>
+            <p class="top-section__merit-desc">&nbsp;&nbsp;セラピータイムは、<b>瞑想動画コンテンツによって、費用を抑えつつ効果的な瞑想</b>を行える瞑想アプリです。<br>&nbsp;&nbsp;<b>高額なお金を払う必要はありません。</b>店舗で行う瞑想は、賃料や人件費がかかりどうしても費用が高くなりますし、独学では正しい方法にたどり着くのに時間がかかりすぎます。<br>&nbsp;&nbsp;これから瞑想を始める方も、すでに取り組んでいる方も、<b>セラピータイムなら専門家の指導下で、月280円〜のお手頃な価格で本格的な瞑想に取り組めます。</b></p>
         </div>
         <div class="top-section__merit-container">
             <h3 class="top-section__merit-title">「いつでも」「どこでも」「何度でも」瞑想できる！</h3>
             <div class="top-section__merit-img-container"><img class="top-section__merit-img--2" src="/img/top-merit2.svg" alt=""></div>
-            <p class="top-section__merit-desc">セラピータイムは、定額制もオンラインサービスです。好きな時に、好きな場所で、好きな回数、瞑想に取り組んでいただけます。瞑想になかなか時間の取れない方も、たくさん時間を割きたい方も、日本にいても海外にいても、好きなだけ瞑想をすることができます。店舗やサロンといった瞑想サービスと比べても、どなたでも非常に取り組みやすいサービスです。</p>
+            <p class="top-section__merit-desc">&nbsp;&nbsp;セラピータイムは、<b>定額制のオンライン瞑想アプリ</b>です。<br>&nbsp;&nbsp;<b>好きな時に、好きな場所で、好きな回数だけ</b>瞑想に取り組んでいただけます。<br>&nbsp;&nbsp;瞑想になかなか時間の取れない方も、たくさん時間を割きたい方も、日本にいても海外にいても、好きなだけ、このアプリの動画コンテンツを通して<b>質の高い瞑想</b>をすることができます。<br>&nbsp;&nbsp;店舗やサロンで行なう瞑想サービスと比べて、<b>どなたでも取り組みやすい瞑想サービスです。</b></p>
         </div>
     </div>
 </section>
@@ -104,12 +101,14 @@
 </section>
 
 <section class="top-section">
-    <h2 class="top-section-title">セラピータイムが選ばれる4つの理由</h2>
+    <h2 class="top-section-title">セラピータイムが選ばれる6つの理由</h2>
     <ul class="top-section__reason-ul">
         <li class="top-section__reason-li">専門家の監修による高品質なコンテンツ</li>
+        <li class="top-section__reason-li">月額280円〜の続けやすい価格</li>
+        <li class="top-section__reason-li">定額制だから回数を気にせず取り組める</li>
+        <li class="top-section__reason-li">動画コンテンツでいつでもどこでも瞑想可能</li>
+        <li class="top-section__reason-li">豊富なコンテンツから、自分にあった瞑想が探せる</li>
         <li class="top-section__reason-li">誰でも使える簡単な操作</li>
-        <li class="top-section__reason-li">月額490円〜の続けやすい価格</li>
-        <li class="top-section__reason-li">瞑想コンテンツが豊富 </li>
     </ul>
     <a href="{{ route('register') }}" class="btn btn-big btn-primary">まずは無料で体験してみる</a>
 
